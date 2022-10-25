64c340bb1b809bf5a9304f3eb7d5bd0c1039d630
AazimAnish <aazimanish1@gmail.com>
2022-10-26 03:32:18 +0530

    initial commit

---
+++ src/Components/Banner.js
@@ -0,0 +1,15 @@
+import { Col, Container, Row } from "react-bootstrap"
+
+export const Banner = () => {
+    return (
+        <section className="banner" id="home">
+          <Container>
+            <Row className="align-items-centre">
+                <Col xs={12} md={6} xl={7}>
+
+                </Col>
+            </Row>
+          </Container>
+        </section>
+    )
+}
